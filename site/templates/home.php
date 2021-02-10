bb226f25b65c7963d921c3d6c24e78fe80d5a2c2
Sofya Tuymedova <stuymedova@Sofyas-MacBook-Air.local>
2021-02-11 02:18:31 +0300

    edits

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,11 +1,9 @@
 <main>
-  <header>
-    <h1><?= $page->title() ?></h1>
-  </header>
+  <h1><?= $page->title() ?></h1>
   <ul>
     <?php foreach ($page->children() as $article): ?>
     <li>
-      <h2><a href='<?= $article->short_url() ?>'><?= $article->title() ?></a></h2>
+      <a href='<?= $article->short_url() ?>'><h2><?= $article->title() ?></h2></a>
       <p><?= $article->byline() ?></p>
       <p><?= $article->abstract() ?></p>
     </li>
